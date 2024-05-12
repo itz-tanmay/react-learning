c3541c295eb7bacfa1cb9cb1f9e94e8364cfd8ce
Tanmay Bhatgare <tanmaybhatgare07@gmail.com>
2024-05-12 14:02:12 +0530

    project 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
 import React, { useState } from "react";
 
-function App() {
-  let [counter, setCounter] = useState(0);
+const App = () => {
+  const [color, setColor] = useState("rgb(24 24 27)");
   return (
-    <>
-      <h1>Hello React</h1>
-      <h2>Counter Value: {counter}</h2>
-      <div>
-        <button
-          onClick={() => {
-            counter < 20 ? setCounter(counter+1) : setCounter(counter);
-          }}
-        >
-          Increase Value
-        </button>
-        <br />
-        <button
-          onClick={() => {
-            counter <= 0 ? setCounter(counter) : setCounter(counter - 1);
-          }}
-        >
-          Decrease Value
-        </button>
+    <div
+      className="w-full h-screen duration-200 flex justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-20">
+        <div className="flex gap-5 bg-red-400 rounded-xl px-4 py-3">
+          <button
+            className="w-20 h-10 bg-red-500 hover:bg-red-600 rounded-full"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+
+          <button
+            className="w-20 h-10 bg-green-500 hover:bg-green-600 rounded-full"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+
+          <button
+            className="w-20 h-10 bg-blue-500 hover:bg-blue-600 rounded-full"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+
+          <button
+            className="w-20 h-10 bg-yellow-500 hover:bg-yellow-600 rounded-full"
+            onClick={() => setColor("yellow")}
+          >
+            Yellow
+          </button>
+
+          <button
+            className="w-20 h-10 bg-slate-300 hover:bg-slate-400 rounded-full"
+            onClick={() => setColor("rgb(203 213 225)")}
+          >
+            Slate
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
