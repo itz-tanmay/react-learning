77ec3fa2cb41b2a7cb724a566c88ed6720d82cd4
Tanmay Bhatgare <tanmaybhatgare07@gmail.com>
2024-05-13 13:38:12 +0530

    project 4 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,81 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 const App = () => {
-  const [length, setLength] = useState(8);
-  const [allowNum, setAllowNum] = useState(false);
-  const [allowChar, setAllowChar] = useState(false);
-  const [password, setPassword] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("usd");
+  const [toCurrency, setToCurrency] = useState("inr");
+  const [resultAmount, setResultAmount] = useState(0);
 
-  const passwordRef = useRef(null);
+  const currencyInfo = useCurrencyInfo(fromCurrency);
 
-  const passwordGenerator = useCallback(() => {
-    let pass = "";
-    let randomStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (allowNum) randomStr += "0123456789";
-    if (allowChar) randomStr += "!@#$%^&*()";
+  const currencyOptions = Object.keys(currencyInfo);
 
-    for (let i = 1; i <= length; i++) {
-      let charIdx = Math.floor(Math.random() * randomStr.length + 1);
-      pass += randomStr.charAt(charIdx);
-    }
+  const swapCurrency = () => {
+    // Swap currencies
+    const tempCurrency = fromCurrency;
+    setFromCurrency(toCurrency);
+    setToCurrency(tempCurrency);
 
-    setPassword(pass);
-  }, [length, allowNum, allowChar, setPassword]);
+    // Swap amounts if needed
+    setAmount(resultAmount);
+    setResultAmount(amount);
+  };
 
-  const copyPassword = useCallback((e) => {
-    passwordRef.current.select();
-    navigator.clipboard.writeText(password);
-    e.target.innerText = "Copied!";
-  }, [password]);
-
-  useEffect(() => {
-    passwordGenerator();
-  }, [length, allowNum, allowChar, passwordGenerator]);
+  const convertFinalAmount = () => {
+    setResultAmount(amount * currencyInfo[toCurrency]);
+  };
 
   return (
-    <>
-      <div className="w-full max-w-lg mx-auto rounded-xl px-5 my-8 bg-zinc-800 text-center mt-10 py-10 text-gray-200">
-        <h1 className="text-2xl text-center text-slate-300 mb-10">
-          Password Generator
-        </h1>
-        <div className="flex overflow-hidden mb-5 rounded-xl gap-x-2">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-2 px-4 my-0 rounded-md text-zinc-900"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
-          />
-          <button
-            className="outline-none bg-blue-400 text-slate-900 px-4 py-0.5 shrink-0 rounded-md"
-            onClick={copyPassword}
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-slate-900">
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convertFinalAmount();
+            }}
           >
-            Copy
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-2">
-          <div className="flex items-center gap-x-1">
-            <input
-              type="range"
-              min={6}
-              max={25}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setLength(e.target.value);
-              }}
-            />
-            <label>Length({length})</label>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={allowNum}
-              id="numberInput"
-              onChange={() => {
-                setAllowNum(!allowNum);
-              }}
-            />
-            <label htmlFor="numberInput">Numbers</label>
-          </div>
-
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={allowChar}
-              id="charInput"
-              onChange={() => {
-                setAllowChar(!allowChar);
-              }}
-            />
-            <label htmlFor="charInput">Character</label>
-          </div>
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={currencyOptions}
+                onCurrencyChange={(currency) => setFromCurrency(currency)}
+                selectCurrency={fromCurrency}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swapCurrency}
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox
+                label="To"
+                amount={resultAmount}
+                currencyOptions={currencyOptions}
+                selectCurrency={toCurrency}
+                onCurrencyChange={(currency) => setToCurrency(currency)}
+                amountDisable
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()}
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
